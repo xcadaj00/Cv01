8010adbf93148d069fc6855ce9bea4f1bad73606
xcadaj00 <xcadaj00@vutbr.cz>
2021-02-11 13:54:17 +0100

    initial

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Cv01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
-
+            Console.WriteLine("New on GitHub.");
 
             Console.ReadLine();
         }
